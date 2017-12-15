8218b2be30cdf0baa31d6b0f1242d506df883fe8
Oleg Stotsky <ostotsky@gmail.com>
2017-12-15 14:42:21 +0000

    add a basic authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    def new
+        
+    end
+    
+    def create
+        user = User.find_by(username: params[:session][:username].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = 'You have successfuly logged in'
+            redirect_to user_path(user)
+        else
+            flash.now[:danger] = "Log in failed. Wrong username or password"
+            render 'new'
+        end
+    end
+    
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to root_path
+    end
+end
