ff54b440d388a3a94593fdc8cc67942f091bc33c
Oleg Stotsky <ostotsky@gmail.com>
2018-07-28 15:04:34 +0000

    Add categories resource

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController
+    def index 
+        @categories = Category.paginate(page: params[:page], per_page: 3)
+    end
+    
+    def show
+    end
+    
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Successfully created new category"
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
