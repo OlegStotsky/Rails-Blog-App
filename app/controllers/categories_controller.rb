eed042d1feb5cb3ec117ad66785701628a498262
Oleg Stotsky <ostotsky@gmail.com>
2018-07-29 18:35:04 +0000

    Feature: category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,8 @@ class CategoriesController < ApplicationController
     end
     
     def show
+        @category = Category.find(params[:id])
+        @articles = @category.articles
     end
     
     def new
